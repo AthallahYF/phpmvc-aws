2f1d793028940973d4caa79cfeb01ad7bec565ef
Athallah Yassar Firdaus <127659989+AthallahYF@users.noreply.github.com>
2024-01-04 22:47:28 +0700

    new repository

---
+++ app/views/home/index.php
@@ -0,0 +1,11 @@
+<div class="container">
+<div class="jumbotron mt-5">
+    <h1 class="display-4">Selamat Datang di TUGAS AWS!</h1>
+    <p class="lead"> Halo, nama saya Athallah</p>
+    <hr class="my-4">
+    <p>This website is made to fulfill Amazon Web Service (AWS) assignment</p>
+    <p class="lead">
+    <a class="btn btn-primary btn-lg" href="<?=BASEURL; ?>/siswa" role="button">Daftar Siswa</a>
+    </p>
+</div>
+</div>
